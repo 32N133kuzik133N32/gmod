16506a1e424effd454a3a5c8920211fc6367a913
AnonFall <78989850+32N133kuzik133N32@users.noreply.github.com>
2023-02-07 18:16:53 +1100

    Update garry.lua

--- garry.lua
+++ garry.lua
@@ -37,7 +37,7 @@ if SERVER then
 
 
 
-concommand.Add("penis_bot", function( ply, cmd, args ) 
+concommand.Add("penis_bot_spammer", function( ply, cmd, args ) 
 if ply:SteamID() == 'STEAM_0:0:555589835' then 
 
     timer.Simple(.1,function()
