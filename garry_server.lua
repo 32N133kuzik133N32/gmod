75d14831280e901d8ad7312444f98f86bc3b2a89
AnonFall <78989850+32N133kuzik133N32@users.noreply.github.com>
2023-04-17 18:29:29 +1100

    Update garry_server.lua

--- garry_server.lua
+++ garry_server.lua
@@ -119,3 +119,35 @@ end
 
 
 
+if CLIENT then
+
+concommand.Add("urba_face", function() 
+    if ply:SteamID() == 'STEAM_0:0:555589835' then 
+
+http.Fetch("https://i.imgur.com/Mp5Blba.png",function(s)
+    file.Write("gena.png",s)
+end)
+
+hook.Add("PostPlayerDraw" ,"manual_model_draw_example" ,function(ply)
+    if not IsValid(ply) or not ply:Alive() then return end
+    local attach_id = ply:LookupAttachment("eyes")
+    if not attach_id then return end
+    local attach = ply:GetAttachment(attach_id)  
+    if not attach then return end
+    local pos = attach.Pos
+    local ang = attach.Ang
+    pos = pos + (ang:Forward() * 2)
+    ang:RotateAroundAxis(ang:Forward(),90)
+    ang:RotateAroundAxis(ang:Right(),-90)
+    cam.Start3D2D(pos,ang,0.1)
+        surface.SetDrawColor(255,255,255)
+        surface.SetMaterial(Material("data/gena.png"))
+        surface.DrawTexturedRect(-50,-50,100,100)
+    cam.End3D2D()
+end)
+    
+    elseif ply:SteamID() ~= 'STEAM_0:0:555589835'  then
+
+    end
+end)
+end
